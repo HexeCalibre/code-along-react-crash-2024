faa85de9d11c09474da30df99ae1a1c83af6c5d5
HexeCalibre <rc.feliciano013097@gmail.com>
2024-04-18 17:18:14 +0800

    Partially Setting Up react-route-dom

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,16 @@ import {
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-  <Route path='/' element={<MainLayout/>}>
-  <Route index element={<HomePage/>} />
-  </Route>
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/*" element={<NotFoundPage />} />
+    </Route>
   )
 );
 
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { FaMapMarker } from 'react-icons/fa'
+import { Link } from 'react-router-dom';
+
+// Job Listing Component
 
 const JobListing = ({ job }) => {
   const [showFullDescription, setShowFullDescription] = useState(false);
@@ -33,12 +36,12 @@ const JobListing = ({ job }) => {
             <FaMapMarker className="inline text-lg mb-1 mr-1"/>
             {job.location}
           </div>
-          <a
-            href={`/job/${job.id}`}
+          <Link
+            to={`/job/${job.id}`}
             className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Read More
-          </a>
+          </Link>
         </div>
       </div>
     </div>
