2c4d7a7d3c9b0dd284aa6314b0b9250afa38da96
HexeCalibre <rc.feliciano013097@gmail.com>
2024-04-23 14:39:37 +0800

    Toastify Implemented

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,22 +1,26 @@
 import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const JobPage = ({ deleteJob }) => {
   const navigate = useNavigate();
   const { id } = useParams();
   const job = useLoaderData();
 
-const onDeleteClick = (jobId) => {
-  const confirm = window.confirm('Are you sure you want to delete this listing');
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this listing"
+    );
 
-  if(!confirm) return;
+    if (!confirm) return;
 
-  deleteJob(jobId);
+    deleteJob(jobId);
 
-  navigate('/jobs');
-  
-}
+    toast.success("Job deleted successfully");
+
+    navigate("/jobs");
+  };
 
   return (
     <>
