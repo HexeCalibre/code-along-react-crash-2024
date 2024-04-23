3b4464492827867d4aa2459b0ee4a31430a8b1eb
HexeCalibre <rc.feliciano013097@gmail.com>
2024-04-23 08:13:34 +0800

    useLoaderData at JobPage.jsx Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import {
   Route,
   createBrowserRouter,
-  createRoutesFromChildren,
   RouterProvider,
   createRoutesFromElements,
 } from "react-router-dom";
@@ -9,14 +8,14 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import JobPage from "./pages/JobPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader}/>
       <Route path="/*" element={<NotFoundPage />} />
     </Route>
   )
